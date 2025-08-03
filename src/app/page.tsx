99a7bfe6e733c13a55c559569f9c569c7842a3c0
Rob Denman <robertkarldenman@gmail.com>
2025-08-03 17:42:11 -0500

    feat: implement google oauth flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <div>Denman Dines</div>;
+import { auth, signIn, signOut } from "@/lib/auth";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <>
+      <h1>Denman Dines</h1>
+
+      {session?.user ? (
+        <>
+          <p>Welcome {session.user.name}!</p>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <button type="submit">Signout</button>
+          </form>
+        </>
+      ) : (
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          <button type="submit">Signin with Google</button>
+        </form>
+      )}
+    </>
+  );
 }
